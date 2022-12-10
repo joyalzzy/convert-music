1deab793e1c37cfc2997526a7d7cf05f80fe03b0
joyalzzy <joyalzzy@gmail.com>
2022-12-10 21:38:39 +0800

    i'm dumb

--- convert.sh
+++ convert.sh
@@ -5,7 +5,7 @@ source=$1
 dest=$2
 
 # Loop through all files in the current directory
-find $1 - type f -name \*.flac  -print0 | while IFS= read -r -d '' file; 
+find $1 -type f -name \*.flac  -print0 | while IFS= read -r -d '' file; 
 do
   # Check if the file is a flac file
   ffmpeg -i "$file" -b:a "${BITRATE}k" "$dest${file#source%.flac}.opus"
